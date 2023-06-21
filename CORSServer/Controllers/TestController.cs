b058660050b588aa5fa65652ae5c5b2b442367ef
REVISION-PC\Seth <sethetherald@duck.com>
2023-06-21 11:50:09 +0700

    Add CORS demo.

---
+++ CORSServer/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CORSServer.Model;
+using Microsoft.AspNetCore.Cors;
+
+namespace CORSServer.Controllers
+{
+    //[EnableCors("MyPolicy")]
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class TestController : Controller
+    {
+        private List<InputModel> _data = new() {
+            new() {Username = "First user", Email = "stunghy@gmail.com"},
+            new() {Username = "Second user", Email = "demomail@xyz.com"},
+            new() {Username = "Third user", Email = "mail@mail.com"}
+        };
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok(_data);
+        }
+    }
+}
---
+++ CORSServer/Model/InputModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CORSServer.Model
+{
+    public class InputModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
